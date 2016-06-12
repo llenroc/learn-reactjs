fd19ebbda11809a0085b64fd62558e068cadb350
Adam Wolf <adam@adamjwolf.com>
2016-06-12 15:15:36 -0700

    Start Chapter 6

---
+++ books/survivejs-webpack/chapter-6/webpack-demo/lib/parts.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+
+      stats: 'errors-only',
+
+      host: options.host,
+      port: options.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({
+        multiStep: true
+      })
+    ]
+  };
+}
+
+exports.setupCSS = function (paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css?modules'],
+          include: paths
+        }
+      ]
+    }
+  };
+}
