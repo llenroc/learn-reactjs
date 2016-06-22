c687389c8eac635737040fd008d5ceee8ed57c2b
Adam Wolf <adam@adamjwolf.com>
2016-06-22 08:34:57 -0700

    Start chapter 8

---
+++ books/survivejs-webpack/chapter-8/webpack-demo/lib/parts.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+
+      stats: 'errors-only',
+
+      host: options.host,
+      port: options.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({
+        multiStep: true
+      })
+    ]
+  };
+}
+
+exports.setupCSS = function (paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css?modules'],
+          include: paths
+        }
+      ]
+    }
+  };
+}
+
+exports.minify = function () {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  };
+}
+
+exports.setFreeVariable = function (key, value) {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  };
+}
